ead00b0290bfabb469d759af33a3248ebffa2173
Sroop Sunar <sroop@users.noreply.github.com>
2014-07-15 18:08:23 +0100

    written a failing test - testing that spares are being calculated correctly in a game

--- spec/scorehistory_spec.rb
+++ spec/scorehistory_spec.rb
@@ -8,7 +8,6 @@ describe 'ScoreHistory' do
   let(:frame2) { Frame.new(2, 1) } 
   let(:frame3) { Frame.new(5, 3) }
   let(:frame4) { Frame.new(6, 2) }
-  let(:frame5) { Frame.new(2, 0) } 
   let(:scoreline) { ScoreLine.new([frame1, frame2, frame3, frame4]) }
   let(:scorehistory) { ScoreHistory.new(scoreline) }
 
@@ -26,6 +25,18 @@ describe 'ScoreHistory' do
 
   it 'can generate a score history - a list of accumulated totals at the end of each frame' do
     expect(scorehistory.show).to eq([7, 10, 18, 26])
+  end
+
+  it 'can correctly calculate the totals at the end of each frame when a spare is rolled' do
+    frame5 = Frame.new(5, 5)
+    frame6 = Frame.new(5, 2)
+    scoreline.add(frame5)
+    expect(scorehistory.rolls).to eq([ [3, 4], [2, 1], [5, 3], [6, 2], [5, 5] ])
+    expect(scorehistory.show).to eq([7, 10, 18, 26, 36])
+    scoreline.add(frame6)
+    expect(scorehistory.rolls).to eq([ [3, 4], [2, 1], [5, 3], [6, 2], [5, 5], [5, 2] ])
+    expect(scorehistory.show).to eq([7, 10, 18, 26, 41, 48])
+
   end
 
 end
