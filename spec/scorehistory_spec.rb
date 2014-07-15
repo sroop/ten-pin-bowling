55e4572e90ec184e5927b7226d8cfe040bac3181
Sroop Sunar <sroop@users.noreply.github.com>
2014-07-15 21:29:51 +0100

    testing for correct calculation of strikes

--- spec/scorehistory_spec.rb
+++ spec/scorehistory_spec.rb
@@ -27,7 +27,7 @@ describe 'ScoreHistory' do
     expect(scorehistory.show).to eq([7, 10, 18, 26])
   end
 
-  it 'can correctly calculate the totals at the end of each frame when a spare is rolled' do
+  it 'can correctly calculate the totals at the end of each frame when a spare is scored' do
     frame5 = Frame.new(5, 5)
     frame6 = Frame.new(4, 2)
     scoreline.add(frame5)
@@ -36,6 +36,14 @@ describe 'ScoreHistory' do
     scoreline.add(frame6)
     expect(scorehistory.rolls).to eq([ [3, 4], [2, 1], [5, 3], [6, 2], [5, 5], [4, 2] ])
     expect(scorehistory.show).to eq([7, 10, 18, 26, 40, 46])
+  end
+
+  it 'can correctly calculate the totals at the end of each frame when a strike is scored' do
+    frame5 = Frame.new(10)
+    frame6 = Frame.new(5, 2)
+    scoreline.add([frame5, frame6])
+    expect(scorehistory.rolls).to eq([ [3, 4], [2, 1], [5, 3], [6, 2], [10, 0], [5, 2] ])
+    expect(scorehistory.show).to eq([ 7, 10, 18, 26, 43, 50 ])
   end
 
 end
